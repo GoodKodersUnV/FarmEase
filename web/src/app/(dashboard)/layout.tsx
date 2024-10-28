2306c4fca115bef250377eae97397965f57be546
Uday Sagar <udaysagar.mail@gmail.com>
2024-10-28 13:01:46 +0530

    authentication and profile page setup

---
+++ web/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import HomeLayout from "@/components/layouts/home-layout";
+import getCurrentUser from "@/actions/getCurrentUser";
+import { User } from "@prisma/client";
+
+export const metadata: Metadata = {
+  title: "VJH",
+  description: "VJH",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const currentUser = await getCurrentUser();
+  return (
+    <HomeLayout currentUser={currentUser as User}>
+      {children}
+    </HomeLayout>
+  );
+}
+
